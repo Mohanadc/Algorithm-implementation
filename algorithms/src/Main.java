1fd19a19e7c4b63681a7eb39aaf6f4f16c9d2da1
EniolaBanjo <eniola.banjo@ucdconnect.ie>
2024-03-28 11:32:40 +0000

    Manual sort

--- algorithms/src/Main.java
+++ algorithms/src/Main.java
@@ -1,17 +1,44 @@
+
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    }
+
+    public static int[] MANUAL_SORT(int[] arr, int low, int high) {
+
+        int N = high-low + 1;
+
+        if (N<=1) {
+            return arr;
         }
+        else if(N==2){
+            if(arr[low]>arr[high]){
+                int j= arr[low];
+                arr[low] = arr[high];
+                arr[high] = j;
+            }
+        }
+        else if(N==3){
+            if(arr[low] > arr[high-1]){
+                int j= arr[low];
+                arr[low] = arr[high-1];
+                arr[high-1] = j;
+            }
+        }
+        if(arr[low]>arr[high]){
+            int j= arr[low];
+            arr[low] = arr[high];
+            arr[high] = j;
+        }
+        if(arr[high-1]>arr[high]){
+            int j= arr[high-1];
+            arr[high-1] = arr[high];
+            arr[high] = j;
+        }
+        return arr;
     }
 }
